ff61cea42ee04fcad2ad4dd094a65cbee7d99fa3
matheus <moraismvgm@gmail.com>
2024-03-17 22:44:16 -0300

    primeiro commit

---
+++ lib/screnns/initial_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../components/task.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacidade = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: const Text('Tarefas'),
+      ),
+      body: AnimatedOpacity(
+        opacity: opacidade ? 1 : 0,
+        duration: const Duration(milliseconds: 800),
+        child: ListView(
+          children: const [
+            Task('Aprender Flutter', 'assets/images/Imagem.png', 5),
+            Task('HTML', 'assets/images/html.png', 4),
+            Task('Correr', 'assets/images/correr.jpg', 3),
+            Task('Ler', 'assets/images/ler.jpg', 2),
+            Task('Meditar', 'assets/images/meditar.jpeg', 1),
+            SizedBox(
+              height: 80,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            opacidade = !opacidade;
+          });
+        },
+        child: const Icon(Icons.remove_red_eye),
+      ),
+    );
+  }
+}
